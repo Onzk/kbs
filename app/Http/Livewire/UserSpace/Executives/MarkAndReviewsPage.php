6cafc34ff458dc07010d835253bfcc3be174360d
OnzK <106268771+Onzk@users.noreply.github.com>
2024-07-04 00:41:59 +0200

    Updates

--- app/Http/Livewire/UserSpace/Executives/MarkAndReviewsPage.php
+++ app/Http/Livewire/UserSpace/Executives/MarkAndReviewsPage.php
@@ -11,6 +11,6 @@ class MarkAndReviewsPage extends Component
         return view('.user-space.executives.mark-and-reviews-page')
             ->extends('user-space.layouts.base')
             ->section('user-space.base.body')
-            ->layoutData(['user_space_title' => 'Profil', 'is_executive' => true]);
+            ->layoutData(['user_space_title' => 'Notes & Avis', 'is_executive' => true]);
     }
 }
